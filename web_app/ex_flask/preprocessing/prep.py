27fa1a7b27f250c1f8240d5004abc05cc4e50eb9
sekR4 <s.kraus@eos-did.com>
2021-06-20 11:09:26 +0200

    shown example outdated, graphs not shown in html

--- web_app/ex_flask/preprocessing/prep.py
+++ web_app/ex_flask/preprocessing/prep.py
@@ -1,11 +1,14 @@
 import pandas as pd
+import numpy as np
+from pathlib import Path
 
 
 def prepared_data():
+    current_dir = Path(__file__).absolute().parent
     df = pd.read_csv(
-        "data/API_SP/API_SP.RUR.TOTL.ZS_DS2_en_csv_v2_2446153.csv", skiprows=4
+        current_dir / "../data/API_SP.RUR.TOTL.ZS_DS2_en_csv_v2_2446153.csv", skiprows=4
     )
-    df = df[["Country Name", "2000", "2019"]]
+
     countrylist = [
         "United States",
         "China",
@@ -20,6 +23,22 @@ def prepared_data():
     ]
     df = df[df["Country Name"].isin(countrylist)]
 
+    cols = df.columns[-12:-2]
+    df.columns[0]
+    cols = list(cols.insert(0, df.columns[0]))
+    df_new = df[cols]
+
+    new_df = {"Country Name": [], "year": [], "prp_rural_pop": []}
+
+    for c in df_new["Country Name"]:
+        prp_rural_pop = np.squeeze(df_new[df_new["Country Name"] == c][cols[1:]].values)
+        new_df["Country Name"].extend([c] * len(prp_rural_pop))
+        new_df["year"].extend(cols[1:])
+        new_df["prp_rural_pop"].extend(prp_rural_pop)
+
+    df_new = pd.DataFrame(new_df)
+
+    df = df[["Country Name", "2000", "2019"]]
     # melt year columns  and convert year to date time
     df_melt = df.melt(id_vars="Country Name", value_vars=["2000", "2019"])
     df_melt.columns = ["country", "year", "variable"]
@@ -36,5 +55,11 @@ def prepared_data():
         x_val = df_melt[df_melt["country"] == country].year.tolist()
         y_val = df_melt[df_melt["country"] == country].percentrural.tolist()
         data.append((country, x_val, y_val))
+    del df, new_df, df_melt, cols
 
-    return data
+    return data, df_new
+
+
+if __name__ == "__main__":
+    _, df = prepared_data()
+    print(df.head(3))
--- web_app/ex_flask/worldbankapp/routes.py
+++ web_app/ex_flask/worldbankapp/routes.py
@@ -1,15 +1,56 @@
-import pandas as pd
-from flask import render_template
+import json
 
-from worldbankapp import app
+import pandas as pd
+import plotly
+import plotly.express as px
+
+import plotly.graph_objects as go
+from flask import render_template
 from preprocessing.prep import prepared_data
 
-data = prepared_data()
-print(data)
+from worldbankapp import app
+
+data, df = prepared_data()
+# print(data)
+
+
+graph_one = []
+for data_tuple in data:
+    graph_one.append(
+        go.Scatter(x=data_tuple[1], y=data_tuple[2], mode="lines", name=data_tuple[0])
+    )
+
+layout_one = dict(
+    title="Some bla <br> ... and bla",
+    xaxis=dict(title="Year", autotick=False, tick0=2000, dtick=25),
+    yaxis=dict(title="Hectares"),
+)
+
+figures = [dict(data=graph_one, layout=layout_one)]
+
+# ids for html tags
+ids = [f"figure-{i}" for i, _ in enumerate(figures)]
+
+# figures to json for javascript in html
+figuresJSON = json.dumps(figures, cls=plotly.utils.PlotlyJSONEncoder)
+
+# NOTE: The code above does not throw errors. However, the HTML page
+# does not show any graph. Let's try it with updated syntax
+
+fig = px.line(
+    df,
+    x="year",
+    y="prp_rural_pop",
+    color="Country Name",
+    title=r"% rural population by country",
+)
+graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
 
 
 @app.route("/")
 @app.route("/index")
 def index():
-    return render_template("index.html", data_set=data)
+    return render_template(
+        "index.html", ids=ids, figuresJSON=figuresJSON, graphJSON=graphJSON
+    )
 
